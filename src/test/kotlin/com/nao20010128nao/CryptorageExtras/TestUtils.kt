c40ff4acb88fda17da9a5d4cab02e4168fa9dcbd
The Hatsune Daishi <nao20010128@gmail.com>
2019-06-04 14:35:11 +0900

    Cryptorageにバグがあった

--- src/test/kotlin/com/nao20010128nao/CryptorageExtras/TestUtils.kt
+++ src/test/kotlin/com/nao20010128nao/CryptorageExtras/TestUtils.kt
@@ -1,3 +1,66 @@
 package com.nao20010128nao.CryptorageExtras
 
+import com.google.common.io.ByteSink
+import com.google.common.io.ByteSource
+import com.nao20010128nao.Cryptorage.Cryptorage
+import com.nao20010128nao.Cryptorage.internal.file.FileSource
+
 val zeroBytes = byteArrayOf()
+
+fun FileSource.fakeWrap(): Cryptorage {
+    return object : Cryptorage {
+        override val isReadOnly: Boolean
+            get() = this@fakeWrap.isReadOnly
+
+        override fun close() {
+            this@fakeWrap.close()
+        }
+
+        override fun commit() {
+            this@fakeWrap.commit()
+        }
+
+        override fun delete(name: String) {
+            this@fakeWrap.delete(name)
+        }
+
+        override fun gc() {
+        }
+
+        override fun lastModified(name: String): Long = this@fakeWrap.lastModified(name)
+
+        override fun list(): Array<String> = this@fakeWrap.list()
+
+        override fun meta(key: String): String? = null
+
+        override fun meta(key: String, value: String) {
+        }
+
+        override fun mv(from: String, to: String) {
+        }
+
+        override fun open(name: String, offset: Int): ByteSource = this@fakeWrap.open(name, offset)
+
+        override fun put(name: String): ByteSink = this@fakeWrap.put(name)
+
+        override fun size(name: String): Long = this@fakeWrap.size(name)
+    }
+}
+
+fun Cryptorage.logged(tag: String? = null): Cryptorage {
+    return object : Cryptorage by this {
+        inline fun <T> w(key: String, name: String, aa: () -> T): T {
+            val response = aa()
+            println("$tag: $key: $name: $response")
+            return response
+        }
+
+        override fun lastModified(name: String): Long = w("lastModified", name) { this@logged.lastModified(name) }
+
+        override fun open(name: String, offset: Int): ByteSource = w("open", name) { this@logged.open(name, offset) }
+
+        override fun put(name: String): ByteSink = w("put", name) { this@logged.put(name) }
+
+        override fun size(name: String): Long = w("size", name) { this@logged.size(name) }
+    }
+}
--- src/test/kotlin/com/nao20010128nao/CryptorageExtras/Tests.kt
+++ src/test/kotlin/com/nao20010128nao/CryptorageExtras/Tests.kt
@@ -1,5 +1,6 @@
 package com.nao20010128nao.CryptorageExtras
 
+import com.nao20010128nao.Cryptorage.combine
 import com.nao20010128nao.Cryptorage.newMemoryFileSource
 import org.junit.Test
 
@@ -7,11 +8,11 @@ class Tests {
     @Test
     fun testPrefixWork() {
         val mem = mapOf(
-            "a-a" to zeroBytes,
-            "a-b" to zeroBytes,
-            "a-c" to zeroBytes,
-            "a-d" to zeroBytes,
-            "b-d" to zeroBytes
+                "a-a" to zeroBytes,
+                "a-b" to zeroBytes,
+                "a-c" to zeroBytes,
+                "a-d" to zeroBytes,
+                "b-d" to zeroBytes
         ).newMemoryFileSource()
         val prefix = mem.withNamePrefixed("a-")
         require(prefix.list().contentEquals(arrayOf("a", "b", "c", "d")))
@@ -20,11 +21,11 @@ class Tests {
     @Test
     fun testSplitsName() {
         val mem = mapOf(
-            "a.000.split" to zeroBytes,
-            "a.001.split" to zeroBytes,
-            "a.002.split" to zeroBytes,
-            "a.003.split" to zeroBytes,
-            "b" to zeroBytes
+                "a.000.split" to zeroBytes,
+                "a.001.split" to zeroBytes,
+                "a.002.split" to zeroBytes,
+                "a.003.split" to zeroBytes,
+                "b" to zeroBytes
         ).newMemoryFileSource()
         val prefix = mem.withSplitFilesCombined()
         require(prefix.list().contentEquals(arrayOf("a", "b")))
@@ -33,10 +34,10 @@ class Tests {
     @Test
     fun testSplitsCombined() {
         val mem = mapOf(
-            "a.000.split" to byteArrayOf(1),
-            "a.001.split" to byteArrayOf(2),
-            "a.002.split" to byteArrayOf(3),
-            "a.003.split" to byteArrayOf(4)
+                "a.000.split" to byteArrayOf(1),
+                "a.001.split" to byteArrayOf(2),
+                "a.002.split" to byteArrayOf(3),
+                "a.003.split" to byteArrayOf(4)
         ).newMemoryFileSource()
         val prefix = mem.withSplitFilesCombined()
         require(byteArrayOf(1, 2, 3, 4).contentEquals(prefix.open("a").read()))
@@ -45,12 +46,43 @@ class Tests {
     @Test
     fun testSplitsCombinedWithSkips() {
         val mem = mapOf(
-            "a.000.split" to byteArrayOf(1),
-            "a.005.split" to byteArrayOf(2),
-            "a.010.split" to byteArrayOf(3),
-            "a.020.split" to byteArrayOf(4)
+                "a.000.split" to byteArrayOf(1),
+                "a.005.split" to byteArrayOf(2),
+                "a.010.split" to byteArrayOf(3),
+                "a.020.split" to byteArrayOf(4)
         ).newMemoryFileSource()
         val prefix = mem.withSplitFilesCombined()
         require(byteArrayOf(1, 2, 3, 4).contentEquals(prefix.open("a").read()))
+    }
+
+    @Test
+    fun testSplitsCombinedSize() {
+        val mem = mapOf(
+                "a.000.split" to byteArrayOf(1),
+                "a.001.split" to byteArrayOf(2),
+                "a.002.split" to byteArrayOf(3),
+                "a.003.split" to byteArrayOf(4),
+                "a.005.split" to byteArrayOf(2),
+                "a.010.split" to byteArrayOf(3),
+                "a.020.split" to byteArrayOf(4)
+        ).newMemoryFileSource()
+        val prefix = mem.withSplitFilesCombined()
+        require(prefix.size("a") == 7L)
+    }
+
+    @Test
+    fun testSplitsCombinedSizeComplicated() {
+        val mem = listOf(
+                "a.000.split" to byteArrayOf(1),
+                "a.001.split" to byteArrayOf(2),
+                "a.002.split" to byteArrayOf(3),
+                "a.003.split" to byteArrayOf(4),
+                "a.005.split" to byteArrayOf(2),
+                "a.010.split" to byteArrayOf(3),
+                "a.020.split" to byteArrayOf(4)
+        ).map { mapOf(it).newMemoryFileSource().fakeWrap() }.combine().logged()
+
+        val prefix = mem.withSplitFilesCombined()
+        require(prefix.size("a") == 7L)
     }
 }
