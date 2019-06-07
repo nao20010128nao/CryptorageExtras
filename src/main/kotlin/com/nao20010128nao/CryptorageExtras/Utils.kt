37e213462ea56fbda7b0bf208746dd75399dbeaa
nao20010128nao <nao20010128@gmail.com>
2019-06-07 14:36:35 +0900

    重要なものを忘れていた

--- src/main/kotlin/com/nao20010128nao/CryptorageExtras/Utils.kt
+++ src/main/kotlin/com/nao20010128nao/CryptorageExtras/Utils.kt
@@ -26,37 +26,30 @@ internal inline fun InputStream.skip(length: Int): InputStream = also {
     ByteStreams.skipFully(this, length.toLong())
 }
 
-fun Cryptorage.logged(tag: String? = null): Cryptorage {
-    return object : Cryptorage by this {
-        inline fun <T> w(key: String, name: String, aa: () -> T): T {
-            val response = aa()
-            println("$tag: $key: $name: $response")
-            return response
-        }
-
-        override fun lastModified(name: String): Long = w("lastModified", name) { this@logged.lastModified(name) }
-
-        override fun open(name: String, offset: Int): ByteSource = w("open", name) { this@logged.open(name, offset) }
-
-        override fun put(name: String): ByteSink = w("put", name) { this@logged.put(name) }
-
-        override fun size(name: String): Long = w("size", name) { this@logged.size(name) }
+fun Cryptorage.logged(tag: String? = null): Cryptorage = object : Cryptorage by this {
+    inline fun <T> w(key: String, name: String, aa: () -> T): T {
+        val response = aa()
+        println("$tag: $key: $name: $response")
+        return response
     }
+
+    override fun lastModified(name: String): Long = w("lastModified", name) { this@logged.lastModified(name) }
+    override fun open(name: String, offset: Int): ByteSource = w("open", name) { this@logged.open(name, offset) }
+    override fun open(name: String): ByteSource = w("open", name) { this@logged.open(name, 0) }
+    override fun put(name: String): ByteSink = w("put", name) { this@logged.put(name) }
+    override fun size(name: String): Long = w("size", name) { this@logged.size(name) }
 }
-fun FileSource.logged(tag: String? = null): FileSource {
-    return object : FileSource by this {
-        inline fun <T> w(key: String, name: String, aa: () -> T): T {
-            val response = aa()
-            println("$tag: $key: $name: $response")
-            return response
-        }
 
-        override fun lastModified(name: String): Long = w("lastModified", name) { this@logged.lastModified(name) }
-
-        override fun open(name: String, offset: Int): ByteSource = w("open", name) { this@logged.open(name, offset) }
-
-        override fun put(name: String): ByteSink = w("put", name) { this@logged.put(name) }
-
-        override fun size(name: String): Long = w("size", name) { this@logged.size(name) }
+fun FileSource.logged(tag: String? = null): FileSource = object : FileSource by this {
+    inline fun <T> w(key: String, name: String, aa: () -> T): T {
+        val response = aa()
+        println("$tag: $key: $name: $response")
+        return response
     }
+
+    override fun lastModified(name: String): Long = w("lastModified", name) { this@logged.lastModified(name) }
+    override fun open(name: String, offset: Int): ByteSource = w("open", name) { this@logged.open(name, offset) }
+    override fun open(name: String): ByteSource = w("open", name) { this@logged.open(name, 0) }
+    override fun put(name: String): ByteSink = w("put", name) { this@logged.put(name) }
+    override fun size(name: String): Long = w("size", name) { this@logged.size(name) }
 }
